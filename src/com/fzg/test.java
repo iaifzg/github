063ef8a889b5bdeeb20aef80dfbfd1e02aadc137
cjn <cjn@qq.com>
2022-04-10 11:33:44 +0800

    master first commit

---
+++ src/com/fzg/test.java
@@ -0,0 +1,13 @@
+package com.fzg;
+
+/**
+ * @author chen on 2022/4/10
+ * @company fzg
+ */
+public class test {
+
+    public static void main(String[] args) {
+        System.out.println("我是老大");
+    }
+
+}
